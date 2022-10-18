f06574b46ed932b86ffec182e09fe659af16a9af
sumanMalware <mukundnayak900@gmial.com>
2022-10-18 17:52:19 +0530

    ui part improved

--- src/App1.js
+++ src/App1.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 import './App1.css'
 import diceimg from './dice.png'
 import snakeladder from './snakeandladder.png'
-// import ladder from './ladder.png'
+import ladder from './ladder.png'
+import snakegif from './snakeGif.gif'
 const App1 = () => {
 
 
-    const refreshPage = ()=>{
+    const refreshPage = () => {
         window.location.reload();
-     }
+    }
 
 
     function Dfr(props) {
@@ -32,9 +33,9 @@ const App1 = () => {
     }
 
     let snakes = [23, 34, 52, 80, 99]
-    let ladders = [10, 16, 21, 35, 44, 85]
-    let toLadders = [33, 37, 41, 54, 70, 90]
-    let toSnakes = [2, 15, 31, 58, 77]
+    let ladders = [10, 21, 35, 67, 77]
+    let toLadders = [33, 42, 56, 89, 98]
+    let toSnakes = [2, 15, 31, 58, 76]
 
     function classAssign(num) {
 
@@ -68,6 +69,7 @@ const App1 = () => {
 
     const diceNumber = () => {
         PlayerTurn()
+
 
         let dice = Math.ceil(Math.random() * 6)
         document.getElementById("display").innerHTML = dice
@@ -107,12 +109,12 @@ const App1 = () => {
             else if (player2 > 100) {
                 setplayer2(player2 - dice)
             }
-            // if(player1===player2){
-            //     document.getElementById(player2).style.background = 'linear-gradient(to right,orange 0%,orange 50%,black 50%,black 100%)'
-
-            // }
-
         }
+        // if(player1===player2){
+        //     document.getElementById(player2).style.background = 'linear-gradient(to right,orange 0%,orange 50%,black 50%,black 100%)'
+
+        // }
+
         else if (turn === "Player 2 Turn") {
             document.getElementById(player2).style.backgroundColor = 'rgb(37, 126, 126)'
 
@@ -151,6 +153,22 @@ const App1 = () => {
     }
     return (
         <div id='main'>
+            {/* <div className="ghj">
+                <h1>Hey</h1>
+                <img src="ladder.png" alt="" />
+            </div> */}
+            <img src={ladder} alt="Ladder" className='ladder1' />
+            <img src={ladder} alt="Ladderr" className='ladder2' />
+            <img src={ladder} alt="Ladderr" className='ladder3' />
+            <img src={ladder} alt="Ladderr" className='ladder4' />
+            <img src={ladder} alt="Ladderr" className='ladder5' />
+            <img src={snakegif} alt="snake" className='snake1' />
+            <img src={snakegif} alt="snake" className='snake2' />
+            <img src={snakegif} alt="snake" className='snake3' />
+            <img src={snakegif} alt="snake" className='snake4' />
+            <img src={snakegif} alt="snake" className='snake5' />
+
+
 
             <div className="gridcdclass">
                 {numberArr.map(value => <div className={classAssign(value)} id={value.toString()} ><Dfr nums={value} />
@@ -173,7 +191,7 @@ const App1 = () => {
                     <div className="dicepoint"><h1 id='display'>0</h1></div>
                     <div className="dicepoint2" ><img src={diceimg} alt="dice" id='diceimg' onClick={diceNumber} /></div>
                 </div>
-                <button onClick={refreshPage} id="refresh"><h2>RESET GAME</h2></button> 
+                <button onClick={refreshPage} id="refresh"><h2>RESET GAME</h2></button>
 
 
 
